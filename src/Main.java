3763540332122401241d5059a2d11384d4dca49a
Jongkwan Choi <doemqjffns97@naver.com>
2022-08-15 00:06:15 +0900

    22.08.14

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,25 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc= new Scanner(System.in);
+		int a=sc.nextInt();
+		int[] arr= new int[1];
+		int sum=0;
 		
-		int a= sc.nextInt();
-		int[] arr= new int[a];
-		ArrayList<Integer> al= new ArrayList();
-		for(int i=0;i<arr.length;i++) {
+		for(int i=0; i<arr.length;i++) {
 			int b= sc.nextInt();
-			double sum=0;
-			double avg=0;
-			double stdavg=0;
-			int std=0;
-			for(int j=0; j<b;j++) {
-			Integer c=sc.nextInt();
-			al.add(c);
-			}
-			for(int j=0; j<al.size();j++) {
-				sum+=al.get(j);
-			}
-			avg=(Double)sum/b;
-			for(int j=0;j<al.size();j++) {
-				if(al.get(j)>avg)
-					std++;
-			}
-			
-			
-			
-
-			stdavg=(double)std/b*100;
-			
-			System.out.printf("%.3f%% \n",stdavg);
-			al.removeAll(al);
+			arr[i]=b;
 		}
-	
+
 		
+		System.out.println(sum);
 		
 		
 	}
 
+
 }
+
+
+		
---
+++ src/문자열/BJ11654.java
@@ -0,0 +1,17 @@
+package 문자열;
+
+import java.util.Scanner;
+
+public class BJ11654 {
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		
+		String a= sc.next();
+		int b= a.charAt(0);
+		
+	
+		System.out.println(b);
+		
+		
+	}
+}
