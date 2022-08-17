683d51dcdeddfa54c5267657f7d0ad28d87fe6f5
Jongkwan Choi <doemqjffns97@naver.com>
2022-08-17 23:29:37 +0900

    2022.08.17

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,7 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc= new Scanner(System.in);
-		int A=sc.nextInt();
-		int B=sc.nextInt();
-		int C=sc.nextInt();
-		int point=0;
-	
-		for(int i=0;;i++) {
-			if((A+B)-(B*i)<=0) {
-			point=i;
-			System.out.println((A+B)-(B*i));
-				break;}
-		}
-		System.out.println(point);
+		
 		
 		
 	}
